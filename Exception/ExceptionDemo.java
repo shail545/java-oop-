8ec264052922224a81446b29878248835ba02b22
shail545 <141555992+shail545@users.noreply.github.com>
2024-12-02 21:33:28 +0000

    container config done

---
+++ Exception/ExceptionDemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age<18){
+            throw new RuntimeException("Sorry you can't vote: ");
+        }else{
+            System.out.println("you can vote");
+        }
+    }
+
+    
+}
